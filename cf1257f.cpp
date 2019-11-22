d5caa26b203b78b3afc8db75dae1b3d933159191
Silenus HK <33480762+btnkij@users.noreply.github.com>
2019-11-22 23:52:34 +0800

    cf1257f
    meet-in-the-middle
    hash

---
+++ cf1257f.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <map>
+#include <cassert>
+using namespace std;
+
+#define INF 0x3f3f3f3f
+typedef long long ll;
+typedef unsigned long long ull;
+
+inline int readi(int& i1) { return scanf("%d", &i1); }
+inline int readi(int& i1, int& i2) { return scanf("%d %d", &i1, &i2); }
+inline int readi(int& i1, int& i2, int& i3) { return scanf("%d %d %d", &i1, &i2, &i3); }
+inline int readi(int& i1, int& i2, int& i3, int& i4) { return scanf("%d %d %d %d", &i1, &i2, &i3, &i4); }
+inline int reads(char* s1) { return scanf("%s", s1); }
+#define clr(mem, val) memset(mem, val, sizeof(mem))
+#define rep(i, begin, end) for (register int i = (begin); i <= (end); i++)
+#define rep2(i1, begin1, end1, i2, begin2, end2) rep(i1, begin1, end1) rep(i2, begin2, end2)
+#define repne(i, begin, end) for (register int i = (begin); i < (end); i++)
+#define repne2(i1, begin1, end1, i2, begin2, end2) repne(i1, begin1, end1) repne(i2, begin2, end2)
+
+int a[110];
+map<vector<int>,int> M;
+int main()
+{
+#ifdef __DEBUG__
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    int n; readi(n);
+    repne(i,0,n)readi(a[i]);
+    repne(i,0,1<<15)
+    {
+        vector<int> diff;
+        int t=__builtin_popcount((a[0]>>15)^i);
+        repne(j,1,n)diff.push_back(__builtin_popcount((a[j]>>15)^i)-t);
+        M.insert(make_pair(diff,i));
+    }
+    int ans=-1;
+    repne(i,0,1<<15)
+    {
+        vector<int> diff;
+        int t=__builtin_popcount((a[0]&0x7FFF)^i);
+        repne(j,1,n)diff.push_back(t-__builtin_popcount((a[j]&0x7FFF)^i));
+        if(M.count(diff))
+        {
+            ans=M[diff]<<15|i;
+            break;
+        }
+    }
+    printf("%d\n",ans);
+    return 0;
+}
