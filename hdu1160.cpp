fae57e79b2e00a078b5454a1af4918bb9dbcc47e
Silenus HK <33480762+btnkij@users.noreply.github.com>
2019-11-28 21:10:01 +0800

    dp

---
+++ hdu1160.cpp
@@ -0,0 +1,78 @@
+/**
+* Number:hdu1160
+* Title:FatMouse's Speed
+* Status:?
+* Tag:[dp]
+**/
+
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <queue>
+#include <cassert>
+using namespace std;
+
+#define INF 0x3f3f3f3f
+typedef long long ll;
+typedef unsigned long long ull;
+
+inline int readi(int& i1) { return scanf("%d", &i1); }
+inline int readi(int& i1, int& i2) { return scanf("%d %d", &i1, &i2); }
+inline int readi(int& i1, int& i2, int& i3) { return scanf("%d %d %d", &i1, &i2, &i3); }
+inline int readi(int& i1, int& i2, int& i3, int& i4) { return scanf("%d %d %d %d", &i1, &i2, &i3, &i4); }
+inline int reads(char* s1) { return scanf("%s", s1); }
+#define clr(mem, val) memset(mem, val, sizeof(mem))
+#define rep(i, begin, end) for (register int i = (begin); i <= (end); i++)
+#define rep2(i1, begin1, end1, i2, begin2, end2) rep(i1, begin1, end1) rep(i2, begin2, end2)
+#define repne(i, begin, end) for (register int i = (begin); i < (end); i++)
+#define repne2(i1, begin1, end1, i2, begin2, end2) repne(i1, begin1, end1) repne(i2, begin2, end2)
+
+struct Node
+{
+    int w,s,id;
+    bool operator<(const Node& rhs)const
+    {
+        return w<rhs.w || w==rhs.w && s<rhs.s;
+    }
+}nodes[100010];
+int dp[100010],pre[100010];
+void show(int k)
+{
+    if(!k)return;
+    show(pre[k]);
+    printf("%d\n",nodes[k].id);
+}
+int main()
+{
+#ifdef __DEBUG__
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    int w,s,n=0;
+    while(readi(w,s)!=EOF)
+    {
+        int id=++n;
+        nodes[id]=(Node){w,s,id};
+    }
+    sort(nodes+1,nodes+n+1);
+    rep(i,1,n)
+    {
+        int t=0,k=0;
+        repne(j,1,i)
+        {
+            if(nodes[j].w<nodes[i].w && nodes[j].s>nodes[i].s)
+            {
+                if(dp[j]>t)t=dp[j],k=j;
+            }
+        }
+        dp[i]=t+1,pre[i]=k;
+    }
+    int* ans=max_element(dp+1,dp+n+1);
+    printf("%d\n",*ans);
+    show(ans-dp);
+    return 0;
+}
---
+++ hdu1260.cpp
@@ -0,0 +1,54 @@
+/**
+* Number:hdu1260
+* Title:Tickets
+* Status:?
+* Tag:[dp, 日期计算]
+**/
+
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <queue>
+#include <cassert>
+using namespace std;
+
+#define INF 0x3f3f3f3f
+typedef long long ll;
+typedef unsigned long long ull;
+
+inline int readi(int& i1) { return scanf("%d", &i1); }
+inline int readi(int& i1, int& i2) { return scanf("%d %d", &i1, &i2); }
+inline int readi(int& i1, int& i2, int& i3) { return scanf("%d %d %d", &i1, &i2, &i3); }
+inline int readi(int& i1, int& i2, int& i3, int& i4) { return scanf("%d %d %d %d", &i1, &i2, &i3, &i4); }
+inline int reads(char* s1) { return scanf("%s", s1); }
+#define clr(mem, val) memset(mem, val, sizeof(mem))
+#define rep(i, begin, end) for (register int i = (begin); i <= (end); i++)
+#define rep2(i1, begin1, end1, i2, begin2, end2) rep(i1, begin1, end1) rep(i2, begin2, end2)
+#define repne(i, begin, end) for (register int i = (begin); i < (end); i++)
+#define repne2(i1, begin1, end1, i2, begin2, end2) repne(i1, begin1, end1) repne(i2, begin2, end2)
+
+int s[2010],d[2010],dp[2010];
+int main()
+{
+#ifdef __DEBUG__
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    int T; readi(T);
+    while(T--)
+    {
+        int n; readi(n);
+        rep(i,1,n)readi(s[i]);
+        rep(i,2,n)readi(d[i]);
+        dp[0]=0, dp[1]=s[1];
+        rep(i,2,n)dp[i]=min(dp[i-2]+d[i],dp[i-1]+s[i]);
+        int hour=8+dp[n]/3600, minute=dp[n]%3600/60, second=dp[n]%60;
+        if(hour<=12)printf("%02d:%02d:%02d am\n",hour,minute,second);
+        else printf("%02d:%02d:%02d pm\n",hour-12,minute,second);
+    }
+    return 0;
+}
