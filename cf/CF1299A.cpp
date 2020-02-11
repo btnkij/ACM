3ffde18ea91068ccb28aa7e060ade2b99cc7e07b
Silenus HK <33480762+btnkij@users.noreply.github.com>
2020-02-11 16:02:53 +0800

    Create CF1299A.cpp

---
+++ cf/CF1299A.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <queue>
+#include <cassert>
+using namespace std;
+
+#define INF 0x3f3f3f3f
+typedef long long ll;
+typedef unsigned long long ull;
+
+inline int readi(int& i1) { return scanf("%d", &i1); }
+inline int readi(int& i1, int& i2) { return scanf("%d %d", &i1, &i2); }
+inline int readi(int& i1, int& i2, int& i3) { return scanf("%d %d %d", &i1, &i2, &i3); }
+inline int readi(int& i1, int& i2, int& i3, int& i4) { return scanf("%d %d %d %d", &i1, &i2, &i3, &i4); }
+inline int reads(char* s1) { return scanf("%s", s1); }
+#define clr(mem, val) memset(mem, val, sizeof(mem))
+#define rep(i, begin, end) for (register int i = (begin); i <= (end); i++)
+#define rep2(i1, begin1, end1, i2, begin2, end2) rep(i1, begin1, end1) rep(i2, begin2, end2)
+#define repne(i, begin, end) for (register int i = (begin); i < (end); i++)
+#define repne2(i1, begin1, end1, i2, begin2, end2) repne(i1, begin1, end1) repne(i2, begin2, end2)
+
+int f(int x,int y)
+{
+    return (x|y)-y;
+}
+int arr[100010];
+int main()
+{
+#ifdef __DEBUG__
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    int n; readi(n);
+    repne(i,0,n)readi(arr[i]);
+    int* ans=nullptr;
+    for(int i=31;i>=0;i--)
+    {
+        int b=1<<i;
+        auto judge=[b](int x){return x&b;};
+        if(count_if(arr,arr+n,judge)==1)
+        {
+            ans=find_if(arr,arr+n,judge);
+            break;
+        }
+    }
+    if(ans)printf("%d ",*ans);
+    repne(i,0,n)if(arr+i!=ans)printf("%d ",arr[i]);
+    return 0;
+}
